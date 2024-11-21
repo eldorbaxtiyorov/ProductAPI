242a59dd40b81e4f7d4ad729b0c2023ef3fe349e
eldor <eldor@MacBook-Pro-4.local>
2024-11-21 19:48:52 +0500

    gitignore is added

--- obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092825d46666a8dd3f243626ef50518ff6054616")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
